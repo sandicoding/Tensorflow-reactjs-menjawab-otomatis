6ac2adea205fc0f70d26d6fdf3d0b65db26580e5
sandi pratama <sandicoding@gmail.com>
2021-09-29 00:29:06 +0700

    upload projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react"
+import "./App.css"
+
+// 0. Install Dependencies
+// 1. Import dependencies
+import * as tf from "@tensorflow/tfjs"
+import * as qna from "@tensorflow-models/qna"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from "react-loader-spinner"
+import { Fragment } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 2. Setup References and state hooks
+    const passageRef = useRef(null)
+    const questionRef = useRef(null)
+    const [answer, setAnswer] = useState()
+    const [model, setModel] = useState(null)
+
+    // 3. Load Tensorflow Model
+    const loadModel = async () => {
+        const loadedModel = await qna.load()
+        setModel(loadedModel)
+        console.log("Model Loaded")
+    }
+
+    useEffect(() => {
+        loadModel()
+    }, [])
+    // 4. Handle Questions
+
+    const answerQuestion = async (e) => {
+        if (e.which === 13 && model !== null) {
+            console.log("Question Submitted.")
+            const passage = passageRef.current.value
+            const question = questionRef.current.value
+
+            const answers = await model.findAnswers(question, passage)
+            setAnswer(answers)
+            console.log(answers)
+        }
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                {model == null ? (
+                    <div>
+                        <div>Model Loading</div>
+                        <Loader type="Puff" color="@00BFFF" height={100} width={100} />
+                    </div>
+                ) : (
+                    <Fragment>
+                        Passage
+                        <textarea ref={passageRef} rows="30" cols="100"></textarea>
+                        Ask a Question
+                        <input ref={questionRef} onKeyPress={answerQuestion} size="80" />
+                        Answers
+                        {answer
+                            ? answer.map((ans, index) => (
+                                  <div>
+                                      <b>Answer {index + 1}</b>
+                                      {ans.text} ({Math.floor(ans.score * 100) / 100})
+                                  </div>
+                              ))
+                            : " "}
+                    </Fragment>
+                )}
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
